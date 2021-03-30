68dd98ee62e41d29a31e720ee0142ad36808c86c
joaomlg <joaomarcos1999jm@gmail.com>
2021-03-29 23:11:30 -0300

    Test mlp

---
+++ tests/test_mlp.py
@@ -0,0 +1,121 @@
+import unittest
+
+import numpy as np
+
+from multilayer_perceptron import MLP
+from multilayer_perceptron.layers import *
+from multilayer_perceptron.activations import *
+from multilayer_perceptron.costs import *
+
+class MLPTestCase(unittest.TestCase):
+  def test_simple_perceptron(self):
+    mlp = MLP()
+    
+    input_layer = InputLayer(3)
+    mlp.add(input_layer)
+
+    output_layer = OutputLayer(1, identity, mean_square_cost)
+    output_layer.weights = np.array([[0.1], [0.2], [0.3]])
+    output_layer.bias = np.array([0.5])
+    mlp.add(output_layer)
+
+    x = np.array([[1, 2, 3]])
+    y = np.array([[2.0]])
+
+    mlp.feedfoward(x)
+
+    np.testing.assert_almost_equal(output_layer.output, np.array([[1.9]]))
+
+    loss = output_layer.calculate_loss(y)
+    np.testing.assert_almost_equal(loss, 0.005)
+
+    mlp.backpropagation(y)
+
+    np.testing.assert_almost_equal(output_layer.gradient, np.array([[-0.1]]))
+
+    mlp.update_weights(0.5)
+
+    np.testing.assert_almost_equal(output_layer.weights, np.array([[0.15], [0.3], [0.45]]))
+    np.testing.assert_almost_equal(output_layer.bias, np.array([0.55]))
+
+  def test_multilayer_perceptron(self):
+    mlp = MLP()
+    
+    input_layer = InputLayer(2)
+    mlp.add(input_layer)
+
+    hidden_layer = DenseLayer(2, tanh)
+    hidden_layer.weights = np.array([[0.5, 0.1], [-0.5, -0.3]])
+    hidden_layer.bias = np.array([-0.2, 0.2])
+    mlp.add(hidden_layer)
+
+    output_layer = OutputLayer(1, identity, mean_square_cost)
+    output_layer.weights = np.array([[0.5], [-0.1]])
+    output_layer.bias = np.array([0.3])
+    mlp.add(output_layer)
+
+    x = np.array([[1.2, 2.5]])
+    y = np.array([[3.0]])
+
+    mlp.feedfoward(x)
+
+    np.testing.assert_almost_equal(output_layer.output, np.array([[-0.005002604047518955]]))
+
+    loss = output_layer.calculate_loss(y)
+    np.testing.assert_almost_equal(loss, 4.5150203251661845)
+
+    mlp.backpropagation(y)
+
+    np.testing.assert_almost_equal(output_layer.gradient, np.array([[-3.005002604047519]]))
+    np.testing.assert_almost_equal(hidden_layer.gradient, np.array([[-0.7849412194740529, 0.25113246595910205]]))
+
+    mlp.update_weights(0.1)
+
+    np.testing.assert_almost_equal(hidden_layer.weights, np.array([[0.5941929463368864, 0.06986410408490776], [-0.30376469513148674, -0.3627831164897755]]))
+    np.testing.assert_almost_equal(hidden_layer.bias, np.array([-0.12150587805259472, 0.1748867534040898]))
+
+    np.testing.assert_almost_equal(output_layer.weights, np.array([[0.2923334443574305], [-0.22179915880877843]]))
+    np.testing.assert_almost_equal(output_layer.bias, np.array([0.6005002604047519]))
+
+  def test_fit_multilayer_perceptron(self):
+    mlp = MLP()
+    
+    input_layer = InputLayer(2)
+    mlp.add(input_layer)
+
+    hidden_layer = DenseLayer(2, tanh)
+    hidden_layer.weights = np.array([[0.5, 0.1], [-0.5, -0.3]])
+    hidden_layer.bias = np.array([-0.2, 0.2])
+    mlp.add(hidden_layer)
+
+    output_layer = OutputLayer(1, identity, mean_square_cost)
+    output_layer.weights = np.array([[0.5], [-0.1]])
+    output_layer.bias = np.array([0.3])
+    mlp.add(output_layer)
+
+    x = np.array([[1.2, 2.5]])
+    y = np.array([[3.0]])
+
+    mlp.fit(
+      train_data=(x, y),
+      epochs=1,
+      learning_rate=0.1,
+      batch_size=1
+    )
+
+    np.testing.assert_almost_equal(output_layer.output, np.array([[-0.005002604047518955]]))
+
+    loss = output_layer.calculate_loss(y)
+    np.testing.assert_almost_equal(loss, 4.5150203251661845)
+
+    np.testing.assert_almost_equal(output_layer.gradient, np.array([[-3.005002604047519]]))
+    np.testing.assert_almost_equal(hidden_layer.gradient, np.array([[-0.7849412194740529, 0.25113246595910205]]))
+
+    np.testing.assert_almost_equal(hidden_layer.weights, np.array([[0.5941929463368864, 0.06986410408490776], [-0.30376469513148674, -0.3627831164897755]]))
+    np.testing.assert_almost_equal(hidden_layer.bias, np.array([-0.12150587805259472, 0.1748867534040898]))
+
+    np.testing.assert_almost_equal(output_layer.weights, np.array([[0.2923334443574305], [-0.22179915880877843]]))
+    np.testing.assert_almost_equal(output_layer.bias, np.array([0.6005002604047519]))
+
+if __name__ == '__main__':
+  unittest.main()
