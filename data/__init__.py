4be366aa2380f37a95a28809d99c36eafc2a0b5f
joaomlg <joaomarcos1999jm@gmail.com>
2021-03-30 20:10:44 -0300

    Check and download dataset if necessary

--- data/__init__.py
+++ data/__init__.py
@@ -2,14 +2,17 @@ import numpy as np
 import gzip
 import pickle
 import os
+import urllib.request
 
 class MNIST:
+  host = 'http://yann.lecun.com/exdb/mnist/'
+
   filenames = {
     'train': ('train-images-idx3-ubyte.gz', 'train-labels-idx1-ubyte.gz'),
     'test': ('t10k-images-idx3-ubyte.gz', 't10k-labels-idx1-ubyte.gz'),
   }
 
-  decoded_filename = 'mnist.pkl.gz'
+  dataset_filename = 'mnist.pkl.gz'
 
   train_samples = 50000
   validation_samples = 10000
@@ -18,8 +21,40 @@ class MNIST:
   def __init__(self):
     self.current_dir = os.path.dirname(__file__)
 
-  def download(self):
-    pass
+    if not self.is_dataset_available():
+      print('Dataset not available! It will be downloaded and decoded, and can be take a while, please wait!')
+      datasets = self.get_base_datasets_filenames()
+
+      for dataset in datasets:
+        if not self.is_base_dataset_downloaded(dataset):
+          print(f'Downloading {dataset}...')
+          self.download_dataset(dataset)
+
+      print('Decoding files and saving it...')
+      self.decode_and_save()
+
+      print('Deleting base files (downloaded)...')
+      for dataset in datasets:
+        self.delete_dataset(dataset)
+    
+      print('Done.')
+
+  def is_dataset_available(self):
+    return os.path.exists(os.path.join(self.current_dir, self.dataset_filename))
+  
+  def get_base_datasets_filenames(self):
+    return self.filenames['train'] + self.filenames['test']
+
+  def is_base_dataset_downloaded(self, filename):
+    return os.path.exists(os.path.join(self.current_dir, filename))
+
+  def download_dataset(self, filename):
+    url = self.host + filename
+    dest = os.path.join(self.current_dir, filename)
+    urllib.request.urlretrieve(url, dest)
+  
+  def delete_dataset(self, filename):
+    os.remove(os.path.join(self.current_dir, filename))
 
   def decode_and_save(self):
     data = {}
@@ -37,10 +72,10 @@ class MNIST:
     validation = tuple(x[self.train_samples:] for x in data['train'])
     test = tuple(data['test'])
 
-    with gzip.open(os.path.join(self.current_dir, self.decoded_filename), 'wb') as file:
+    with gzip.open(os.path.join(self.current_dir, self.dataset_filename), 'wb') as file:
       pickle.dump((training, validation, test), file)
 
   def load(self):
-    with gzip.open(os.path.join(self.current_dir, self.decoded_filename), 'rb') as file:
+    with gzip.open(os.path.join(self.current_dir, self.dataset_filename), 'rb') as file:
       training, validation, test = pickle.load(file)
     return training, validation, test
