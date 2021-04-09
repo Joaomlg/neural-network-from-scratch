469dc0802e331c1b691dd4d4693116869ed47f97
joaomlg <joaomarcos1999jm@gmail.com>
2021-04-08 23:20:46 -0300

    Abstract layer with input and output shape property

--- multilayer_perceptron/layers.py
+++ multilayer_perceptron/layers.py
@@ -3,8 +3,40 @@ import numpy as np
 class Layer:
   def __init__(self):
     self.prev_layer = None
-    self.output = None
     self.next_layer = None
+    self.output = None
+    self.gradient = None
+
+  def set_previus_layer(self, layer: 'Layer'):
+    if not isinstance(layer, Layer):
+      raise TypeError(f'Layer should be "{type(Layer)}" type, not "{type(layer)}"')
+    self.prev_layer = layer
+
+  def set_next_layer(self, layer: 'Layer'):
+    if not isinstance(layer, Layer):
+      raise TypeError(f'Layer should be "{type(Layer)}" type, not "{type(layer)}"')
+    self.next_layer = layer
+  
+  def setup(self):
+    raise NotImplementedError
+  
+  @property
+  def input_shape(self):
+    if self.prev_layer is None:
+      raise Exception('Previous layer not assigned.')
+    else:
+      return self.prev_layer.output_shape
+  
+  @property
+  def output_shape(self):
+    raise NotImplementedError
+  
+  @property
+  def input_data(self):
+    if self.prev_layer is None:
+      raise Exception('Previous layer not assigned.')
+    else:
+      return self.prev_layer.output
 
   def foward(self):
     raise NotImplementedError
@@ -13,34 +45,56 @@ class Layer:
     raise NotImplementedError
 
 class InputLayer(Layer):
-  def __init__(self, size):
-    self.size = size
+  def __init__(self, shape: (int, tuple)):
     super().__init__()
+    self.shape = shape
   
+  def setup(self):
+    pass
+
+  @property
+  def input_shape(self):
+    if isinstance(self.shape, int):
+      return (1, self.shape)
+    else:
+      return self.shape
+  
+  @property
+  def output_shape(self):
+    return self.input_shape
+
   def foward(self, input_data):
     self.output = input_data
 
 class DenseLayer(Layer):
-  def __init__(self, size, activation, drop_probability=0):
-    self.size = size
+  def __init__(self, num_of_neurons, activation, drop_probability=0):
+    super().__init__()
+    self.num_of_neurons = num_of_neurons
     self.activation = activation
     self.drop_probability = drop_probability
-    self.gradient = None
     self.weights = None
     self.bias = None
     self.pre_activation = None
-    super().__init__()
+  
+  @property
+  def output_shape(self):
+    return (1, self.num_of_neurons)
+  
+  def setup(self):
+    self.initialize_weights()
+    self.initialize_bias()
 
   def initialize_weights(self):
-    weights_shape = self.prev_layer.size, self.size
+    input_rows, input_cols = self.input_shape
+    weights_shape = (input_cols, self.num_of_neurons)
     self.weights = np.random.uniform(-0.5, 0.5, weights_shape)
 
   def initialize_bias(self):
-    bias_size = self.size
+    bias_size = self.num_of_neurons
     self.bias = np.random.uniform(-0.5, 0.5, bias_size)
 
   def foward(self):
-    self.pre_activation = (self.prev_layer.output @ self.weights) + self.bias
+    self.pre_activation = (self.input_data @ self.weights) + self.bias
     self.output = self.activation(self.pre_activation)
     if self.drop_probability:
       keep_prob = 1 - self.drop_probability
@@ -52,7 +106,7 @@ class DenseLayer(Layer):
     self.gradient = (self.next_layer.gradient @ self.next_layer.weights.T) * self.activation(self.pre_activation, derivative=True)
   
   def update_weights(self, learning_rate):
-    self.weights -= learning_rate * (self.prev_layer.output.T @ self.gradient)
+    self.weights -= learning_rate * (self.input_data.T @ self.gradient)
     self.bias -= learning_rate * self.gradient.mean(axis=0)
 
 class OutputLayer(DenseLayer):
