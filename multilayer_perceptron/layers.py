f52ffc7d91015ac8a12c94c0af995570b26ef88a
joaomlg <joaomarcos1999jm@gmail.com>
2021-04-10 11:38:13 -0300

    MaxPoolingLayer implementation

--- multilayer_perceptron/layers.py
+++ multilayer_perceptron/layers.py
@@ -204,3 +204,57 @@ class ConvolutionalLayer(Layer):
             gradient_slice = self.gradient[sample, :, j:j+self.kernel_height, i:i+self.kernel_width]
             gradient_slice += self.next_layer.gradient[kernel, j, i] * self.kernels[kernel]
 
+class MaxPoolingLayer(Layer):
+  def __init__(self, pool_size: tuple, stride=(1, 1)):
+    super().__init__()
+    self.pool_size = self.pool_height, self.pool_width = pool_size
+    self.stride = self.vertical_stride, self.horizontal_stride = stride
+
+  def setup(self):
+    pass
+
+  @property
+  def output_height(self):
+    input_channels, input_height, input_width = self.input_shape
+    return 1 + (input_height - self.pool_height) // self.vertical_stride
+  
+  @property
+  def output_width(self):
+    input_channels, input_height, input_width = self.input_shape
+    return 1 + (input_width - self.pool_width) // self.horizontal_stride
+
+  @property
+  def output_shape(self):
+    input_channels, input_height, input_width = self.input_shape
+    return (input_channels, self.output_height, self.output_width)
+
+  def foward(self):
+    input_samples, input_channels, input_height, input_width = self.input_data.shape
+
+    self.output = np.zeros((input_samples, input_channels, self.output_height, self.output_width))
+
+    for sample in range(input_samples):
+      for channel in range(input_channels):
+        for j in range(self.output_height):
+          for i in range(self.output_width):
+            x_min, y_min = i * self.horizontal_stride, j * self.vertical_stride
+            x_max, y_max = x_min + self.pool_width, y_min + self.pool_height
+            input_slice = self.input_data[sample, channel, y_min:y_max, x_min:x_max]
+            pooling_result = np.max(input_slice)
+            self.output[sample, channel, j, i] = pooling_result
+
+  def backward(self):
+    input_samples, input_channels, input_height, input_width = self.input_data.shape
+
+    self.gradient = np.zeros_like(self.input_data)
+
+    for sample in range(input_samples):
+      for channel in range(input_channels):
+        for j in range(self.output_height):
+          for i in range(self.output_width):
+            x_min, y_min = i * self.horizontal_stride, j * self.vertical_stride
+            x_max, y_max = x_min + self.pool_width, y_min + self.pool_height
+            input_slice = self.input_data[sample, channel, y_min:y_max, x_min:x_max]
+            mask = (input_slice == self.output[sample, channel, j, i])
+            gradient_slice = self.gradient[sample, channel, y_min:y_max, x_min:x_max]
+            gradient_slice += mask * self.next_layer.gradient[sample, channel, j, i]
---
+++ tests/test_pool_layer.py
@@ -0,0 +1,104 @@
+import unittest
+
+import numpy as np
+
+from multilayer_perceptron.layers import *
+from multilayer_perceptron.activations import *
+
+class PoolingLayerTestCase(unittest.TestCase):
+  def test_pooling_layer_output_shape(self):
+    input_layer = InputLayer((3, 4, 4))
+    pool_layer = MaxPoolingLayer(
+      pool_size=(2, 2),
+      stride=(2, 2)
+    )
+
+    pool_layer.set_previus_layer(input_layer)
+
+    expected_output_shape = (3, 2, 2)
+    np.testing.assert_almost_equal(pool_layer.output_shape, expected_output_shape)
+  
+  def test_pooling_layer_foward(self):
+    input_layer = InputLayer((2, 4, 4))
+    pool_layer = MaxPoolingLayer(
+      pool_size=(2, 2),
+      stride=(2, 2)
+    )
+
+    pool_layer.set_previus_layer(input_layer)
+
+    input_data = np.array([[[[0.1 , 0.2 , 0.3 , 0.5 ],
+                             [0.3 , 0.2 , 0.4 , 0.1 ],
+                             [0.4 , 0.3 , 0.2 , 1.  ],
+                             [0.1 , 0.1 , 0.2 , 0.5 ]],
+
+                            [[0.5 , 0.3 , 0.2 , 0.3 ],
+                             [0.9 , 0.5 , 0.3 , 0.98],
+                             [0.4 , 0.3 , 0.2 , 0.1 ],
+                             [0.5 , 0.55, 0.43, 0.21]]]])
+    
+    input_layer.output = input_data
+
+    pool_layer.foward()
+
+    expected_output = np.array([[[[0.3 , 0.5 ],
+                                  [0.4 , 1.  ]],
+
+                                 [[0.9 , 0.98],
+                                  [0.55, 0.43]]]])
+
+    np.testing.assert_almost_equal(pool_layer.output, expected_output)
+
+  def test_pooling_layer_backward(self):
+    input_layer = InputLayer((2, 4, 4))
+
+    pool_layer = MaxPoolingLayer(
+      pool_size=(2, 2),
+      stride=(2, 2)
+    )
+
+    output_layer = Layer()
+
+    pool_layer.set_previus_layer(input_layer)
+    pool_layer.set_next_layer(output_layer)
+
+    input_data = np.array([[[[0.1 , 0.2 , 0.3 , 0.5 ],
+                             [0.3 , 0.2 , 0.4 , 0.1 ],
+                             [0.4 , 0.3 , 0.2 , 1.  ],
+                             [0.1 , 0.1 , 0.2 , 0.5 ]],
+
+                            [[0.5 , 0.3 , 0.2 , 0.3 ],
+                             [0.9 , 0.5 , 0.3 , 0.98],
+                             [0.4 , 0.3 , 0.2 , 0.1 ],
+                             [0.5 , 0.55, 0.43, 0.21]]]])
+    
+    input_layer.output = input_data
+
+    pool_layer.output = np.array([[[[0.3 , 0.5 ],
+                                    [0.4 , 1.  ]],
+
+                                   [[0.9 , 0.98],
+                                    [0.55, 0.43]]]])
+
+    output_layer.gradient = np.array([[[[0.1, 0.2],
+                                        [0.3, 0.4]],
+
+                                       [[0.5, 0.6],
+                                        [0.7, 0.8]]]])
+
+    pool_layer.backward()
+
+    expected_input_gradient = np.array([[[[0.0, 0.0, 0.0, 0.2],
+                                          [0.1, 0.0, 0.0, 0.0],
+                                          [0.3, 0.0, 0.0, 0.4],
+                                          [0.0, 0.0, 0.0, 0.0]],
+
+                                         [[0.0, 0.0, 0.0, 0.0],
+                                          [0.5, 0.0, 0.0, 0.6],
+                                          [0.0, 0.0, 0.0, 0.0],
+                                          [0.0, 0.7, 0.8, 0.0]]]])
+
+    np.testing.assert_almost_equal(pool_layer.gradient, expected_input_gradient)
+
+if __name__ == '__main__':
+  unittest.main()
