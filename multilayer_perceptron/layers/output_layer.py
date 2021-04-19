OutputLayer backward gradients fixed

multilayer_perceptron/layers/output_layer.py
@@ -9,9 +9,10 @@ class OutputLayer(DenseLayer):
     super().__init__(num_of_neurons, activation)
 
   def backward(self, target):
-    self.output_gradient = self.cost(self.output, target, derivative=True) * self.activation(self.pre_activation, derivative=True)
-    self.gradient = self.input_data.T @ self.output_gradient
-    self.bias_gradient = self.output_gradient.mean(axis=0)
+    pre_activation_gradient = self.cost(self.output, target, derivative=True) * self.activation(self.pre_activation, derivative=True)
+    self.weight_gradient = self.input_data.T @ pre_activation_gradient
+    self.bias_gradient = pre_activation_gradient.sum(axis=0)
+    self.gradient = pre_activation_gradient @ self.weights.T
 
   def calculate_loss(self, target):
     return self.cost(self.output, target)