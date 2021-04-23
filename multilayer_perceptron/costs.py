91349abf36c46ae999bd80f210cfeba0d9fda010
joaomlg <joaomarcos1999jm@gmail.com>
2021-04-22 22:19:07 -0300

    Clip predict with a smaller range

--- multilayer_perceptron/costs.py
+++ multilayer_perceptron/costs.py
@@ -6,14 +6,14 @@ def mean_square_cost(predict, target, derivative=False):
   return (1/2) * np.sum((target - predict) ** 2) / len(target)
 
 def cross_entropy_cost(predict, target, derivative=False):
-  clipped_predict = np.clip(predict, 1e-20, 1 - 1e-20)
+  clipped_predict = np.clip(predict, 1e-15, 1 - 1e-15)
   if derivative:
     return -1 * (target / clipped_predict)
   else:
     return -1 * np.sum(target * np.log(clipped_predict)) / len(target)
 
 def binary_cross_entropy_cost(predict, target, derivative=False):
-  clipped_predict = np.clip(predict, 1e-20, 1 - 1e-20)
+  clipped_predict = np.clip(predict, 1e-15, 1 - 1e-15)
   if derivative:
     return -1 * ((target / clipped_predict) - ((1 - target) / (1 - clipped_predict)))
   else:
