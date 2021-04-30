2ab5181916460c0cca00ee59863079db2b8dea8e
joaomlg <joaomarcos1999jm@gmail.com>
2021-04-29 21:13:11 -0300

    Max pool for loops reduced

--- multilayer_perceptron/layers/max_pooling_layer.py
+++ multilayer_perceptron/layers/max_pooling_layer.py
@@ -31,28 +31,23 @@ class MaxPoolingLayer(Layer):
 
     self.output = np.zeros((input_samples, input_channels, self.output_height, self.output_width))
 
-    for sample in range(input_samples):
-      for channel in range(input_channels):
-        for j in range(self.output_height):
-          for i in range(self.output_width):
-            x_min, y_min = i * self.horizontal_stride, j * self.vertical_stride
-            x_max, y_max = x_min + self.pool_width, y_min + self.pool_height
-            input_slice = self.input_data[sample, channel, y_min:y_max, x_min:x_max]
-            pooling_result = np.max(input_slice)
-            self.output[sample, channel, j, i] = pooling_result
+    for j in range(self.output_height):
+      for i in range(self.output_width):
+        x_min, y_min = i * self.horizontal_stride, j * self.vertical_stride
+        x_max, y_max = x_min + self.pool_width, y_min + self.pool_height
+        input_slice = self.input_data[:, :, y_min:y_max, x_min:x_max]
+        self.output[:, :, j, i] = np.max(input_slice, axis=(2, 3))
 
   def backward(self):
     input_samples, input_channels, input_height, input_width = self.input_data.shape
 
     self.gradient = np.zeros_like(self.input_data)
 
-    for sample in range(input_samples):
-      for channel in range(input_channels):
-        for j in range(self.output_height):
-          for i in range(self.output_width):
-            x_min, y_min = i * self.horizontal_stride, j * self.vertical_stride
-            x_max, y_max = x_min + self.pool_width, y_min + self.pool_height
-            input_slice = self.input_data[sample, channel, y_min:y_max, x_min:x_max]
-            mask = (input_slice == self.output[sample, channel, j, i])
-            gradient_slice = self.gradient[sample, channel, y_min:y_max, x_min:x_max]
-            gradient_slice += mask * self.next_layer.gradient[sample, channel, j, i]
+    for j in range(self.output_height):
+      for i in range(self.output_width):
+        x_min, y_min = i * self.horizontal_stride, j * self.vertical_stride
+        x_max, y_max = x_min + self.pool_width, y_min + self.pool_height
+        input_slice = self.input_data[:, :, y_min:y_max, x_min:x_max]
+        mask = (input_slice.reshape((input_samples, input_channels, self.pool_height*self.pool_width)) == self.output[:, :, j, i, np.newaxis])
+        gradient_slice = self.gradient[:, :, y_min:y_max, x_min:x_max]
+        gradient_slice += (mask * self.next_layer.gradient[:, :, j, i, np.newaxis]).reshape(gradient_slice.shape)
