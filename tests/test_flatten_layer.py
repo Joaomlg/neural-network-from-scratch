140a7201c6be2a752bc88be406c633438fda8241
joaomlg <joaomarcos1999jm@gmail.com>
2021-04-29 21:31:09 -0300

    Flatten layer test improved with more samples

--- tests/test_flatten_layer.py
+++ tests/test_flatten_layer.py
@@ -18,30 +18,34 @@ class FlattenLayerTestCase(unittest.TestCase):
     np.testing.assert_almost_equal(flatten_layer.output_shape, expected_output_shape)
   
   def test_flatten_layer_foward(self):
-    input_layer = InputLayer((3, 2, 2))
+    input_layer = InputLayer((1, 2, 2))
     flatten_layer = FlattenLayer()
 
     flatten_layer.set_previus_layer(input_layer)
 
-    input_data = np.array([[[[0. , 0.1],
-                             [0.2, 0.3]],
+    input_data = np.array([[[[0.89738036, 0.98673454],
+                             [0.55129726, 0.8030491 ]]],
 
-                            [[0.4, 0.5],
-                             [0.6, 0.7]],
 
-                            [[0.8, 0.9],
-                             [1. , 1.1]]]])
+                           [[[0.62816706, 0.05484155],
+                             [0.18796328, 0.98746524]]],
+
+
+                           [[[0.63243453, 0.88908661],
+                             [0.48268783, 0.88405604]]]])
     
     input_layer.output = input_data
 
     flatten_layer.foward()
 
-    expected_output = np.array([[0. , 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1. , 1.1]])
+    expected_output = np.array([[0.89738036, 0.98673454, 0.55129726, 0.8030491 ],
+                                [0.62816706, 0.05484155, 0.18796328, 0.98746524],
+                                [0.63243453, 0.88908661, 0.48268783, 0.88405604]])
 
     np.testing.assert_almost_equal(flatten_layer.output, expected_output)
 
   def test_flatten_layer_backward(self):
-    input_layer = InputLayer((3, 2, 2))
+    input_layer = InputLayer((1, 2, 2))
 
     flatten_layer = FlattenLayer()
 
@@ -50,29 +54,35 @@ class FlattenLayerTestCase(unittest.TestCase):
     flatten_layer.set_previus_layer(input_layer)
     flatten_layer.set_next_layer(output_layer)
 
-    input_data = np.array([[[[0. , 0.1],
-                             [0.2, 0.3]],
+    input_data = np.array([[[[0.89738036, 0.98673454],
+                             [0.55129726, 0.8030491 ]]],
 
-                            [[0.4, 0.5],
-                             [0.6, 0.7]],
 
-                            [[0.8, 0.9],
-                             [1. , 1.1]]]])
+                           [[[0.62816706, 0.05484155],
+                             [0.18796328, 0.98746524]]],
+
+
+                           [[[0.63243453, 0.88908661],
+                             [0.48268783, 0.88405604]]]])
     
     input_layer.output = input_data
 
-    output_layer.gradient = np.array([[0., 0.11, 0.22, 0.33, 0.44, 0.55, 0.66, 0.77, 0.88, 0.99, 1.1, 1.21]])
+    output_layer.gradient = np.array([[0.06494434, 0.1065572 , 0.61667899, 0.69901242],
+                                      [0.57754459, 0.39518983, 0.52322838, 0.34086176],
+                                      [0.6649398 , 0.4820081 , 0.14890696, 0.66733853]])
 
     flatten_layer.backward()
 
-    expected_input_gradient = np.array([[[[0.  , 0.11],
-                                          [0.22, 0.33]],
+    expected_input_gradient = np.array([[[[0.06494434, 0.1065572 ],
+                                          [0.61667899, 0.69901242]]],
 
-                                         [[0.44, 0.55],
-                                          [0.66, 0.77]],
 
-                                         [[0.88, 0.99],
-                                          [1.1 , 1.21]]]])
+                                        [[[0.57754459, 0.39518983],
+                                          [0.52322838, 0.34086176]]],
+
+
+                                        [[[0.6649398 , 0.4820081 ],
+                                          [0.14890696, 0.66733853]]]])
 
     np.testing.assert_almost_equal(flatten_layer.gradient, expected_input_gradient)
 
