fb105e2f7be9dc6b0b1d5356bd8a1228b0d881f1
joaomlg <joaomarcos1999jm@gmail.com>
2021-05-04 21:12:52 -0300

    Costs changed to class

--- multilayer_perceptron/costs.py
+++ multilayer_perceptron/costs.py
@@ -1,20 +1,46 @@
 import numpy as np
+from abc import ABC, abstractmethod
 
-def mean_square_cost(predict, target, derivative=False):
-  if derivative:
+
+class AbstractCost(ABC):
+  @abstractmethod
+  def loss(self, predict: np.array, target: np.array) -> float:
+    raise NotImplementedError
+  
+  @abstractmethod
+  def gradient(self, predict: np.array, target: np.array) -> np.array:
+    raise NotImplementedError
+
+
+class MeanSquareError(AbstractCost):
+  def loss(self, predict: np.array, target: np.array) -> np.array:
+    return (1/2) * np.sum((target - predict) ** 2) / len(target)
+  
+  def gradient(self, predict: np.array, target: np.array) -> np.array:
     return -1 * (target - predict)
-  return (1/2) * np.sum((target - predict) ** 2) / len(target)
 
-def cross_entropy_cost(predict, target, derivative=False):
-  clipped_predict = np.clip(predict, 1e-15, 1 - 1e-15)
-  if derivative:
-    return -1 * (target / clipped_predict)
-  else:
+
+class CrossEntropyCost(AbstractCost):
+  def __init__(self, limit=1e-15):
+    self.limit = limit
+
+  def loss(self, predict: np.array, target: np.array) -> np.array:
+    clipped_predict = np.clip(predict, limit, 1 - limit)
     return -1 * np.sum(target * np.log(clipped_predict)) / len(target)
+  
+  def gradient(self, predict: np.array, target: np.array) -> np.array:
+    clipped_predict = np.clip(predict, limit, 1 - limit)
+    return -1 * (target / clipped_predict)
 
-def binary_cross_entropy_cost(predict, target, derivative=False):
-  clipped_predict = np.clip(predict, 1e-15, 1 - 1e-15)
-  if derivative:
-    return -1 * ((target / clipped_predict) - ((1 - target) / (1 - clipped_predict)))
-  else:
+
+class BinaryCrossEntropyCost(AbstractCost):
+  def __init__(self, limit=1e-15):
+    self.limit = limit
+
+  def loss(self, predict: np.array, target: np.array) -> np.array:
+    clipped_predict = np.clip(predict, 1e-15, 1 - 1e-15)
     return -1 * np.sum(target * np.log(clipped_predict) + (1 - target) * np.log(1 - clipped_predict)) / len(target)
+  
+  def gradient(self, predict: np.array, target: np.array) -> np.array:
+    clipped_predict = np.clip(predict, 1e-15, 1 - 1e-15)
+    return -1 * ((target / clipped_predict) - ((1 - target) / (1 - clipped_predict)))
